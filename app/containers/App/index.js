9595eaca4f5e465ce8316ca4c87602fd0bf0d4fa
Derrick Cross <decross1@me.com>
2018-04-01 10:50:46 -0700

    Removed helmet from the homepage

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,13 +7,14 @@
  */
 
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
+// Import Pages used for Routing
 import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+
+// Importing Headers and Footers
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
@@ -29,16 +30,9 @@ const AppWrapper = styled.div`
 export default function App() {
   return (
     <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
-      >
-        <meta name="description" content="A React.js Boilerplate application" />
-      </Helmet>
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
       <Footer />
